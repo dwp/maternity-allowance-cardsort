b16793d296df788365c6a7e9425b1f1e2d8e09e8
Jonathan Thomas <jonathanthomas83@gmail.com>
2021-08-25 13:06:03 +0100

    Started Cardsort prototype and initiated routes file

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,16 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/about-you', function (req, res) {
+  res.redirect('work');
+});
+
+router.post('/work', function (req, res) {
+  res.redirect('test-period');
+});
+
+router.post('/test-period', function (req, res) {
+  res.redirect('employment-and-earnings');
+});
+
 module.exports = router
